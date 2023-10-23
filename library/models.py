00acecec037c05ffe7ed6d5859562dc9ba6ab74f
sztabamacia <92019704+sztabamacia@users.noreply.github.com>
2023-10-23 12:56:51 +0200

    Add files via upload

---
+++ library/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Klient(models.Model):
+    idKlient=models.IntegerField(auto_created=True)
+    imie = models.CharField(max_length=100, blank=True, null= True, default="")
+    nazwisko = models.CharField(max_length=100, blank=True, null=True, default="")
+    idWyporzyczenia = models.ForeignKey("Wypozyczenie", on_delete=models.CASCADE)
+    adres = models.CharField(max_length=200)
+    email = models.EmailField()
+    nrtel = models.IntegerField()
+
+
+class Wypozyczenie(models.Model):
+    idWypozyczenia = models.IntegerField(auto_created=True)
+    dataWypozyczenia = models.DateField()
+    dataZwrotu = models.DateField()
+    idKsiazki = models.ForeignKey("Ksiazki", on_delete=models.CASCADE)
+
+
+class Ksiazki(models.Model):
+    idKsiazki = models.IntegerField(auto_created=True)
+    tytul = models.CharField(max_length=100)
+    autorID = models.ForeignKey("Autorzy", on_delete=models.CASCADE)
+    rokWydania = models.IntegerField()
+    dostepnosc = models.BooleanField()
+
+class Autorzy(models.Model):
+    autorID = models.IntegerField(auto_created=True)
+    imie = models.CharField(max_length=100, blank=True, null=True, default="")
+    nazwisko = models.CharField(max_length=100, blank=True, null=True, default="")
