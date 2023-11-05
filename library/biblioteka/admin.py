3fd65047aa53850417a3d681738d0ed5da9b983e
sztabamacia <jddisy@gmail.com>
2023-11-05 19:33:01 +0100

    commit całego projektu do momentu rest

--- library/biblioteka/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(Klient)
-admin.site.register(Wypozyczenie)
-admin.site.register(Autorzy)
-admin.site.register(Ksiazki)
--- library/biblioteka/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BibliotekaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'biblioteka'
